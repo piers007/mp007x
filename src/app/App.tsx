6670031e56a0774620ef4a66c30967c221a5d872
piers007 <mpiers@gmail.com>
2026-01-01 23:35:21 -0500

    Wrap DashboardPage in a div container
    Update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
-import { DashboardPage } from '../pages/DashboardPage'
+// src/app/App.tsx
+import React from "react";
+import DashboardPage from "../pages/DashboardPage";
 
 export default function App() {
-  return <DashboardPage />
+  return (
+    <div className="knox-shell">
+      <DashboardPage />
+    </div>
+  );
 }
