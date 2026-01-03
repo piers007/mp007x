a057aca5ca2ae837170b5e9bc78a81ac80d10958
piers007 <mpiers@gmail.com>
2026-01-02 21:34:52 -0500

    Update start.sh
    Modulae

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
-echo "Starting Knox 007 Backend..."
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
+set -e
+exec uvicorn main:app --host 0.0.0.0 --port "${PORT:-10000}"
